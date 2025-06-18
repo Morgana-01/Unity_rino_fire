9b23823af3b24bedb6394017de4c0fc7d3a09ead
Morgana-01 <178003115+Morgana-01@users.noreply.github.com>
2025-06-18 19:59:18 +0200

    Add movement and timer logic to rino_0

--- Assets/rino_0.cs
+++ Assets/rino_0.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class rino_0 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // A rhinó mozgási sebessége (egység/másodperc)
+    public float sebesseg = 5f;
+
+    // Ennyi idő múlva álljon meg a rhinó (másodpercben)
+    public float megallasIdozito = 5f;
+
+    // Meghatározza, hogy a rhinó éppen mozog-e
+    private bool mozog = true;
+
+    // Start egyszer fut le, mielőtt az első Update meghívódik
     void Start()
     {
-        
+        // Itt most nincs speciális inicializálás
     }
 
-    // Update is called once per frame
+    // Update minden frame-ben lefut
     void Update()
     {
-        
+        if (mozog)
+        {
+            // Előre mozgatjuk az objektumot a beállított sebességgel
+            transform.position += Vector3.forward * sebesseg * Time.deltaTime;
+
+            // Folyamatosan csökkentjük a megállási időzítőt
+            megallasIdozito -= Time.deltaTime;
+
+            // Ha az időzítő lejárt, megállítjuk a mozgást
+            if (megallasIdozito <= 0f)
+            {
+                mozog = false;
+            }
+        }
     }
 }
